912a15023cbb03aef2adf6737cc12dfed6710f13
TylerTeifke <tylerteifke@gmail.com>
2025-08-31 10:26:49 -0700

    Added the ability to change the background color.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ADAM from './Images/ADAM.png';
 import ADAM_smug from './Images/ADAM_smug.png'
 import ADAM_confused from './Images/ADAM_confused.png'
+import ADAM_angry from './Images/ADAM_angry.png'
 import TextBox from './TextBox';
 
 function App() {
   //Will store the information for each text box
   const textBoxxes = [
     {text: "Hello user! I am the Advanced Developmental Automated Machine. But you can call me ADAM. Click me to see something cool.", color: "black", image: ADAM},
-    {text: "See, pretty cool.", color: "red", image: ADAM_smug},
-    {text: "What? You don't seem very impressed.", color: "blue", image:ADAM_confused}
+    {text: "See, pretty cool.", color: "green", image: ADAM_smug},
+    {text: "What? You don't seem very impressed.", color: "blue", image: ADAM_confused},
+    {text: "Alright then, how about this.", color: "red", image: ADAM_angry},
+    {text: "Now I bet your impressed.", color: "green", image: ADAM_smug}
   ]
 
   //Will be used to iterate between text boxes
   const [currentBox, setCurrentBox] = useState(0)
+  //Will be used to change the color of the background
+  const [bgColor, setBgColor] = useState('white')
+
+  //Will run whenever there is a change made to the background color
+  useEffect(() => {
+    document.body.style.backgroundColor = bgColor
+  }, [bgColor])
 
   //Will change the text box upon clicking ADAM
   const handleClick = () => {
     if(currentBox === textBoxxes.length - 1){
+      setBgColor('white')
       setCurrentBox(0)
     }
     else{
+      if(currentBox === 3){
+        setBgColor('black')
+      }
       setCurrentBox(currentBox + 1)
     }
   }
